a7058f045e22d5c9ebe729be0bae599b2d522c42
Rob88es <151527880+Rob88es@users.noreply.github.com>
2024-03-11 15:12:04 +0100

    Add files via upload

---
+++ checkpoint3/Checkpoint3.py
@@ -0,0 +1,37 @@
+1.
+string = "hello world"
+number = 5
+list = [1, 2, 3]
+boolean = 3 > 2
+
+2.
+exercise_2 = string[0:3]
+print(exercise_2)
+
+3.
+exercise_3 = list[0]
+print(exercise_3)
+
+4.
+exercise_4 = number + 10
+print(exercise_4)
+
+5.
+exercise_5 = list[-1]
+print(exercise_5)
+
+6.
+names = 'harry,alex,susie,jared,gail,conner'
+list_names = names.split(',')
+print(list_names)
+
+7.
+exercise_7 = string[0:5].upper() + string [5:-1]
+print(exercise_7)
+
+8.
+exercise_8 = f"{string} hoy es {number}"
+print(exercise_8)
+
+9. 
+print(string)
