c548e018587d9cc466caebba52596d50fbc00a14
Rob88es <151527880+Rob88es@users.noreply.github.com>
2024-03-22 21:06:09 +0100

    Add files via upload

--- Checkpoint_5/Link _DailySmarty.py
+++ Checkpoint_5/Link _DailySmarty.py
@@ -1,19 +1,16 @@
 import requests
-import inflection
+from inflection import titleize
 from bs4 import BeautifulSoup
 
 r = requests.get('https://www.dailysmarty.com/topics/python')
 soup = BeautifulSoup(r.text, 'html.parser')
+
 titulos_post = soup.find_all('a')
 
 for titulo in titulos_post:
-  url = titulo['href']
-
-  if url.startswith('/posts/'):
+  url = titulo.get('href')
+  if url and url.startswith('/posts/'):
     texto_del_url = url[7:]
-
     texto = texto_del_url.replace('-', ' ')
-
-    texto_final = inflection.titleize(texto)
-
+    texto_final = titleize(texto)
     print(texto_final)
