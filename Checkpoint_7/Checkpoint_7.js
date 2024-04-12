8737e0259e46977b94a700c8d696a1dee991a3c4
Rob88es <151527880+Rob88es@users.noreply.github.com>
2024-04-12 16:47:38 +0200

    Add files via upload

---
+++ Checkpoint_7/Checkpoint_7.js
@@ -0,0 +1,10 @@
+const numeros = (a, b, c, d) => {
+    const sumaNumeros = (a + b) * (c + d);
+  
+    if (sumaNumeros > 50) {
+      console.log("¡El número es mayor que 50!");
+    } else {
+      console.log("¡El número es menor que 50!");
+    }
+  };
+
